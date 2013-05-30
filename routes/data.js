1d5c21941386dd002aca1ee57f9498288d9728e9
Andrew Ringler <ringler+github@gmail.com>
2013-05-29 21:04:04 -0400

    actually plotting on the calendar view

--- routes/data.js
+++ routes/data.js
@@ -36,16 +36,15 @@ exports.dailyfuel = function(req, res) {
                     }
                 }
 
-                console.log(' Data ' + data.length);
+                var csv = 'Date,Value\n';
                 for (var i = 0; i < data.length; i++) {
                     var dateForDay = data[i];
-                    console.log(dateForDay.date + ' ' + dateForDay.value);
+                    csv += dateForDay.date + ',' + dateForDay.value + '\n';
                 }
 
-                var body = 'Date,Open,High,Low,Close,Volume,Adj Close\n' + '2010-10-01,10789.72,10907.41,10759.14,10829.68,4298910000,10829.68\n' + '2010-09-30,10835.96,10960.99,10732.27,10788.05,4284160000,10788.05\n' + '2010-09-29,10857.98,10901.96,10759.75,10835.28,3990280000,10835.28\n' + '2010-09-28,10809.85,10905.44,10714.03,10858.14,4025840000,10858.14\n' + '2010-09-27,10860.03,10902.52,10776.44,10812.04,3587860000,10812.04';
                 res.setHeader('Content-Type', 'text/csv');
-                res.setHeader('Content-Length', body.length);
-                res.end(body);
+                res.setHeader('Content-Length', csv.length);
+                res.end(csv);
             },
             response: function(response) {
                 console.log("Oh no!");
