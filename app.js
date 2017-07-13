41b3215d32130bd79372758d3d9ba60afd783337
Andrew Ringler <andrew@andrewringler.com>
2017-07-13 17:35:33 -0400

    router

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'hjs');
 //app.use(express.logger('dev'));
 // app.use(express.bodyParser());
 // app.use(express.methodOverride());
-app.use(app.router);
+// app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
